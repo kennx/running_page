f7df332fec9205a4502cb53c24dea4de89857659
zhangjian <zhangxjian@me.com>
2021-07-02 14:41:46 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   pathPrefix: `/`, // Change to `/running_page` when running on github pages
   siteMetadata: {
     title: 'Running page',
-    siteUrl: 'https://yihong.run',
-    description: 'Personal site and blog',
+    siteUrl: 'https://kennx.github.io/running_page',
+    description: 'Running page',
   },
   plugins: [
     'gatsby-transformer-json',
